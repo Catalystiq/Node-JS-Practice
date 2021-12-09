6081d2859416329661a800b1be0359fcb952f41c
Catalystiq <73408095+Catalystiq@users.noreply.github.com>
2021-12-08 21:56:57 -0700

    updated schema to store document on articles collection
    Updated schema to store document on articles collection

--- models/blog.js
+++ models/blog.js
@@ -16,5 +16,5 @@ const blogSchema = new Schema({
     }
 }, { timestamps: true });
 
-const Blog = mongoose.model('Blog', blogSchema);
+const Blog = mongoose.model('articles', blogSchema);
 module.exports = Blog;
